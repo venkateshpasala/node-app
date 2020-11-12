a0096791999407fbead8f6ee9d1362fac318fff9
Aashish Kumar <kumaraashish@facileconsulting.com>
2020-11-12 11:26:43 +0530

    changes for test deployement

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", function (req, res) {
  res.send("Welcome to Jenkins CI and CD");
 });
 
-//listen to port 2020 by default
-app.listen(process.env.PORT || 2020);
-console.log('Node server running on port 2020');
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+console.log('Node server running on port 3000');
 module.exports = app;
